81c8add6b1944761337f9278216670671fab9e23
Princess <princess@pop-os.localdomain>
2024-12-24 19:44:25 +0200

    update

---
+++ backend/server_side/game_level.py
@@ -0,0 +1,28 @@
+class GameLevel():
+ 
+    def __init__(self, level,win  , num_length , guess_range ):
+        self.level = level
+        self.win =  win
+        self.num_length = num_length
+        self.guess_range = guess_range
+        
+    def get_level(self):
+        return self.level
+    
+    def get_win_boolean(self):
+        return self.win
+    
+    def get_num_length(self):
+        return self.num_length
+    
+    def get_guess_range(self):
+        return self.guess_range
+
+    def level_Updater(self):
+          if self.win :
+            self.level += 1
+
+    # def guess_updater(self):
+         
+        
+    
