93cb6507452ff298db3f2a6e2ad106abbab22449
pmambambo023 <pmambambo023@student.wethinkcode.co.za>
2024-12-24 19:46:32 +0200

    update

---
+++ backend/server_side/leader_board.py
@@ -0,0 +1,11 @@
+class LeaderBoard():
+
+    def __init__(self , score , username):
+        self.score = score
+        self.username = username
+
+    def __str__(self):
+        #pull and push into a database
+        pass
+        
+    
