a68e94029e7281369387c8d407dc657a62626d01
pmambambo023 <pmambambo023@student.wethinkcode.co.za>
2024-12-27 03:13:20 +0200

    added more functionality

--- backend/server_side/random_num_gen.py
+++ backend/server_side/random_num_gen.py
@@ -1,20 +1,21 @@
 import random
 
 class GuessNumGenarator():
+    guess_list = []
 
     def __init__(self, guess_length , range_length):
         self.guess_length = guess_length
         self.range_length = range_length
-        self.guess_list = []
+        
 
 
     def random_number(self):
         for num  in range(self.guess_length):
             random_num = random.randint(self.range_length)
             self.guess_list.__add__(random_num)
-        return self.guess_list
+      
     
     def string_guess(self):
-        num_list= self.guess_list
+        return ",".join(self.guess_list)
         
             
