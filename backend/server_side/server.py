2413712a54fd4b5c15279028e5af2df944e77aed
Fungi023 <princessfungie01@gmail.com>
2025-03-28 16:30:36 +0200

    setting up server connection , day one

--- backend/server_side/server.py
+++ backend/server_side/server.py
@@ -1,6 +1,24 @@
 import socket
+import threading
 
 class Server():
-    
-    def __init__(self):
+    port = 5050
+    server = socket.gethostbyname(socket.gethostname())
+
+    def __init__(self, port , server):
+        self.port = port
+        self.server = server
+        self.addr = (server , port)
+
+    def client_handler(self):
         pass
+
+    def start(self):
+        server_conn =  socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+        server_conn.bind(self.addr)
+        server_conn.listen()
+        while True :
+            conn , address = server_conn.accept()
+            thread = threading.Thread(target= self.client_handler() , args= (conn , address) )
+            thread.start()
+            print(f"ACTIVE CONNECTIONS  {threading.active_count()}")
